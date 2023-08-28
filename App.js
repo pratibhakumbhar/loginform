0d72242f081df5cb71595077c679ef5cf9aa22a1
Pratibha Kumbhar <pratibha.kumbhar@splus.local>
2023-08-28 13:36:56 +0530

    FirstCommit

---
+++ App.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('Login-Form');
+
+ 
+
+form.addEventListener('submit', function(event){
+
+    event.preventDefault();
+
+ 
+
+    const username = document.getElementById('Username').value;
+
+    const password = document.getElementById('Password').value;
+
+ 
+
+    if(username ==="Yadynesh" && password ==="Systems2023"){
+
+        window.location.href="About.html";
+
+    }
+
+    else{
+
+        document.getElementById("error-msg").textContent="Invalid username or password";
+
+    }
+
+});
